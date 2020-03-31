7e504513b408a52a2d377f6387b60ca9e42be6da
Вячеслав <macbook@MacBook-Pro.local>
2020-03-31 13:18:06 +0300

    First deploy

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import re
 API_TOKEN = '853778348:AAFSMMu5Lvz9_Jg1j9P7L-efBu3ay1RzPKs'
 bot = telebot.TeleBot(API_TOKEN)
 
-f = open('/content/sample_data/listmsk.txt', 'r')   # здесь лежит файл со станциями:
+f = open('/files/listmsk.txt', 'r')   # здесь лежит файл со станциями:
 content = f.readlines()
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
   poisk=str(message.text)
   print(poisk)
-  ans = ''
+  ans = 'Ваш запрос содержится в станциях:\n'
   shablon = ''
   for i in range(0,len(poisk)):
     shablon = shablon+poisk[i]
@@ -21,7 +21,7 @@ def echo_message(message):
     mat = re.search(shablon, content[i], re.I)
     if mat:
       ans=ans+'\n'+content[i][0:len(content[i])-1]
-  if ans=='':
+  if ans=='Ваш запрос содержится в станциях:\n':
     ans='Запрос не содержится ни в одной из станций :('
   bot.reply_to(message, ans.format(message.text))
   print(ans)
