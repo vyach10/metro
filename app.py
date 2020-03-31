3a009ce8b0f0d026c6e21571d320297bf39d1e84
Вячеслав <macbook@MacBook-Pro.local>
2020-03-31 14:43:30 +0300

    First deploy

--- app.py
+++ app.py
@@ -10,19 +10,22 @@ content = f.readlines()
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
   poisk=str(message.text)
+  if poisk=='start/':
+  	bot.reply_to(message, 'Приветствую! Введите имя или слово для поиска.'.format(message.text))
   print(poisk)
-  ans = 'Ваш запрос содержится в станциях:\n'
-  shablon = ''
-  for i in range(0,len(poisk)):
-    shablon = shablon+poisk[i]
-    shablon = shablon+'.'
-    shablon = shablon+'*'
-  for i in range(0,len(content)):
-    mat = re.search(shablon, content[i], re.I)
-    if mat:
-      ans=ans+'\n'+content[i][0:len(content[i])-1]
-  if ans=='Ваш запрос содержится в станциях:\n':
-    ans='Запрос не содержится ни в одной из станций :('
-  bot.reply_to(message, ans.format(message.text))
+  elif:
+  	ans = 'Ваш запрос содержится в станциях:\n'
+  	shablon = ''
+  		for i in range(0,len(poisk)):
+    		shablon = shablon+poisk[i]
+    		shablon = shablon+'.'
+    		shablon = shablon+'*'
+  		for i in range(0,len(content)):
+    		mat = re.search(shablon, content[i], re.I)
+    	if mat:
+      		ans=ans+'\n'+content[i][0:len(content[i])-1]
+  		if ans=='Ваш запрос содержится в станциях:\n':
+    	ans='Запрос не содержится ни в одной из станций :('
+  	bot.reply_to(message, ans.format(message.text))
   print(ans)
 bot.polling()
