76e802b044f64fde97699a6bcb3d3a8067d8b4e4
Вячеслав <macbook@MacBook-Pro.local>
2020-04-02 22:53:15 +0300

    First commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import re
 API_TOKEN = '853778348:AAFSMMu5Lvz9_Jg1j9P7L-efBu3ay1RzPKs'
 bot = telebot.TeleBot(API_TOKEN)
 
-place='moscow'
+place=''
 f = open('files/'+place+'.txt', 'r')   # здесь лежит файл со станциями:
 dicti = f.readlines()
 
