b1dc2ecc2a46e248fbf11b460161b2e9f5fb2635
Вячеслав <macbook@MacBook-Pro.local>
2020-04-05 04:48:14 +0300

    First commit

--- app.py
+++ app.py
@@ -22,16 +22,18 @@ with open(path) as f:
 #-------------------------------------------------------------
 place=''
 c=''
-temp=''
+temp=''   # для нового города
+tmes={'content_type': 'text', 'message_id': 0000, 'from_user': {'id': 000000000, 'is_bot': False, 'first_name': 'x', 'username': 'x', 'last_name': 'x', 'language_code': 'en'}, 'date': 0000000000, 'chat': {'type': 'private', 'last_name': 'x', 'first_name': 'x', 'username': 'x', 'id': 000000000, 'title': None, 'all_members_are_administrators': None, 'photo': None, 'description': None, 'invite_link': None, 'pinned_message': None, 'sticker_set_name': None, 'can_set_sticker_set': None}, 'forward_from_chat': None, 'forward_from_message_id': None, 'forward_from': None, 'forward_date': None, 'reply_to_message': None, 'edit_date': None, 'media_group_id': None, 'author_signature': None, 'text': '1', 'entities': None, 'caption_entities': None, 'audio': None, 'document': None, 'photo': None, 'sticker': None, 'video': None, 'video_note': None, 'voice': None, 'caption': None, 'contact': None, 'location': None, 'venue': None, 'animation': None, 'new_chat_member': None, 'new_chat_members': None, 'left_chat_member': None, 'new_chat_title': None, 'new_chat_photo': None, 'delete_chat_photo': None, 'group_chat_created': None, 'supergroup_chat_created': None, 'channel_chat_created': None, 'migrate_to_chat_id': None, 'migrate_from_chat_id': None, 'pinned_message': None, 'invoice': None, 'successful_payment': None, 'connected_website': None, 'json': {'message_id': 0000, 'from': {'id': 000000000, 'is_bot': False, 'first_name': 'x', 'x': 'x', 'language_code': 'en'}, 'chat': {'id': 000000000, 'first_name': 'x', 'username': 'x', 'type': 'private'}, 'date': 0000000000, 'text': '1'}}
+tmes2=tmes
 
-@bot.message_handler(commands=['start'])          # команда /start
+@bot.message_handler(commands=['start', 'Сменить город'])          # команда /start
 def start(message):
     kb_start = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn_a = types.KeyboardButton('/start')
+    btn_a = types.KeyboardButton('Сменить город')
     btn_pic = types.KeyboardButton('Показать пример')
+#   btn_lnk   # ссылка на меня в инстике    
     kb_start.add(btn_a,btn_pic)
 #------------------------------------------------------------
-    bot.send_message(message.from_user.id, text='Предлагаем вызглянуть на метро под другим углом', reply_markup=kb_start)
     keyboard = types.InlineKeyboardMarkup(row_width=3);
     key_msk = types.InlineKeyboardButton(text='Москва', callback_data='moscow');
     key_stp = types.InlineKeyboardButton(text='Санкт-Петербург', callback_data='saintp');
@@ -45,20 +47,14 @@ def start(message):
     keyboard.add(key_stp);  # добавляем кнопку Питера в клавиатуру
     keyboard.add(key_nsk, key_ekb, key_niz);  # добавляем кнопки в клавиатуру
     keyboard.add(key_kzn, key_sam, key_vlg);  # добавляем кнопку в клавиатуру
+    bot.send_message(message.from_user.id, text='Хеш-тег #ИменаМетро', reply_markup=kb_start)
     bot.send_message(message.from_user.id, text='Выберите город:', reply_markup=keyboard)
 #------------------------------------------------------------   
 
-def change_city(place, newplace, c, city, message):
-    global temp
-    temp = message
-    #bot.send_message(chat_id, ans)
-    markup = types.InlineKeyboardMarkup(row_width=3);
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data=newplace);
-    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no');
-    markup.add(key_yes, key_no);  # добавляем кнопки Да и Нет в клавиатуру
-    message = bot.send_message(message.from_user.id, text='В городе '+c+' запрос не найден, но найден в городе '+city+'. Сменить город?', reply_markup=markup)
-
 def poisk(word, message):
+    global temp
+    global tmes
+    global tmes2
     ans = ''
     shablon = ''
     for i in range(0, len(word)):
@@ -76,7 +72,13 @@ def poisk(word, message):
           newplace = j['place']
           city = j['city']
       if ans != '':
-        change_city(place, newplace, c, city, message)
+        markup = types.InlineKeyboardMarkup(row_width=3);
+        temp = newplace
+        tmes2 = message
+        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');
+        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no');
+        markup.add(key_yes, key_no);  # добавляем кнопки Да и Нет в клавиатуру
+        message = bot.send_message(message.from_user.id, text='В городе '+c+' запрос не найден, но найден в городе '+city+'. Сменить город?', reply_markup=markup)
         ans = 'False'
     if ans == '':
       ans = 'Запрос не содержится ни в одной из станций'
@@ -84,7 +86,9 @@ def poisk(word, message):
 
 @bot.message_handler(func=lambda message: True)      
 def city(message):
-    if str(message.text) == 'Показать пример':
+    if str(message.text) == 'Сменить город':
+      start(message)
+    elif str(message.text) == 'Показать пример':
       dig=str(randint(1,8))
       photo = open('pic/'+dig+'.JPG', 'rb')
       bot.send_photo(message.from_user.id, photo)
@@ -108,27 +112,23 @@ def city(message):
 
 @bot.callback_query_handler(func=lambda call: True)  # обработчик клавиатуры
 def callback_worker(call):
-  if call == 'no':
-    msg1 = bot.send_message(call.message.chat.id, 'Введите запрос для поиска:')
+  global place
+  global c
+  if call.message and (call.data == 'yes' or call.data == 'no'):
+    if call.data == 'yes':
+      place = temp
+      for k in data:
+        if k['place'] == place:
+          c = k['city']
+      bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Установлен город: '+c)
+      city(tmes2)
+    if call.data == 'no':
+      bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Введите запрос для поиска:');
   else:    
-    global place, c
     place = call.data
     for k in data:
       if k['place'] == place:
         c = k['city']
-    bot.send_message(call.message.chat.id, 'Установлен город: '+c+'\nЧтобы выбрать другой город, отправьте /start')
-    msg1 = bot.send_message(call.message.chat.id, 'Введите запрос для поиска:')
-
-def callback_worker(call, ):
-  if call == 'yes':
-    msg1 = bot.send_message(call.message.chat.id, 'Введите запрос для поиска:')
-  else:    
-    global place, c
-    place = call.data
-    for k in data:
-      if k['place'] == place:
-        c = k['city']
-    bot.send_message(call.message.chat.id, 'Установлен город: '+c+'\nЧтобы выбрать другой город, отправьте /start')
-    msg1 = bot.send_message(call.message.chat.id, 'Введите запрос для поиска:')
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Установлен город: '+c+'\nВведите запрос для поиска:')
 
 bot.polling()
