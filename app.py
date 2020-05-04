fe5071dc64864b7a78e146a8a37456767a8ebd79
Вячеслав <macbook@MacBook-Pro.local>
2020-05-04 20:13:35 +0300

    First commit

--- app.py
+++ app.py
@@ -136,12 +136,12 @@ def callback_worker(call):
 def change_city(id, new_city):
   cursor = connect.cursor()
   connect.cursor()
-  a=cursor.execute('select city from city_bd where id=%s', (id))
+  a=cursor.execute("select city from city_bd where id=%s", (id))
   print(a, '\n')
   if a:
     cursor.execute("update city_bd set city = '%s' WHERE id = %s", (new_city,id))
-  else
-    cursor.execute('insert into city_bd (id, city) values (%s, %s)', (id, new_city)
+  else:
+    cursor.execute("insert into city_bd (id, city) values (%s, %s)", (id, new_city)
   connect.commit()
   cursor.close()
 
