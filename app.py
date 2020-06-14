7f1f761705ec05121b48b3e28e40a30f08e57c0d
Вячеслав <macbook@MacBook-Pro.local>
2020-06-14 19:15:38 +0300

    First commit

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def callback_worker(call):
         c = k['city']
     change_city(call.message.chat.id, place)
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Установлен город: '+c)
-    city(records)
+    city(records[0])
 
   elif call.data == 'no':
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Введите запрос для поиска:');
